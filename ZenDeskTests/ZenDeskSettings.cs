93dbc28db15bc226fd453868ac6e9c7beed18471
Eric Neifert <eric.neifert@gmail.com>
2011-02-07 14:52:43 -0500

    added sample settings file

---
+++ ZenDeskTests/ZenDeskSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZenDeskTests
+{
+    public class ZenDeskSettings
+    {
+        public const string Site = "http://yoursite.zendesk.com";
+        public const string Email = "youremail";
+        public const string EndUserEmail = "sample end user email";
+        public const string Password = "password";
+        public const string TwitterName = "yourtwittername";
+
+    }
+}
