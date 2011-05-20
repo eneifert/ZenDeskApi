fc915835cfcfdc7b3feef30a62fba621797810a0
Eric Neifert <eric.neifert@gmail.com>
2011-05-20 08:49:37 -0400

    Added possible RestSharp dependency

--- ZenDeskTests/ZenDeskSettings.cs
+++ ZenDeskTests/ZenDeskSettings.cs
@@ -8,9 +8,9 @@ namespace ZenDeskTests
     public class ZenDeskSettings
     {
         public const string Site = "http://yoursite.zendesk.com";
-        public const string Email = "adminemail";
-        public const string EndUserEmail = "useremail";
-        public const string Password = "password";
+        public const string Email = "email";
+        public const string EndUserEmail = "some end user email";
+        public const string Password = "your password";
         public const string TwitterName = "twitter";
         public const int CustomFieldId = 481028;
         public const string ViewName = "My unsolved tickets";
